f691af9156eb82248aff88d68b7caeed65b0a551
BradC <brad.chaiken@gmail.com>
2017-04-09 13:15:30 -0400

    edits and updates

--- src/main/java/com/chaikenintegrations/spring/tutorial/mail/MockMailSender.java
+++ src/main/java/com/chaikenintegrations/spring/tutorial/mail/MockMailSender.java
@@ -1,13 +1,12 @@
 package com.chaikenintegrations.spring.tutorial.mail;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MockMailSender implements MailSender {
 
-    private static final Log log = LogFactory.getLog(MockMailSender.class);
+    private static final org.apache.juli.logging.Log log = LogFactory.getLog(MockMailSender.class);
 
     @Override
     public void send(String to, String subject, String body) {
